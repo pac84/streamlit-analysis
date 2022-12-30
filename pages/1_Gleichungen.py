fb02f8464b5b34e22ff9cb9492ce2dbaa33d85ef
m-pagel <pagel@max-planck-gymnasium.de>
2022-12-30 22:58:12 +0100

    dezimalzahlen

--- pages/1_Gleichungen.py
+++ pages/1_Gleichungen.py
@@ -9,6 +9,7 @@ st.header("Lösen von Gleichungen")
 st.markdown("""
 Gib eine Gleichung in das Textfeld ein, achte auf folgende Hinweise:
 - Nutze 'x' als Variable.
+- Dezimalzahlen müssen mit einem '.' statt mit einem ',' eingegeben werden, also '2.3' für $2,3$.
 - Potenzen werden mit \*\* eingegeben, also z.B. x\*\*2 für $x^2$.
 - Zwischen Faktoren und der Variablen muss ein '\*' stehen, also 2\*x statt 2x.
 - Die Gleichung muss auf '= 0' umgestellt sein und es wird nur eine Seite der Gleichung eingegeben, also statt 2\*x = 3 muss 2\*x - 3 eingegeben werden.
