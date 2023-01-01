af2db82f54d7a0fc6095b07209c698611a44b7b7
m-pagel <pagel@max-planck-gymnasium.de>
2023-01-01 14:59:59 +0100

    image

--- Startseite.py
+++ Startseite.py
@@ -2,6 +2,9 @@ import streamlit as st
 import sympy as sp
 import numpy as np
 import matplotlib as plt
+from PIL import Image
+
+image = Image.open('images/intro.png')
 
 st.set_page_config(
     page_title="Analysis",
@@ -12,6 +15,8 @@ st.write("# Tools für Analysis")
 
 st.markdown(
     """
-    Hier finden sich einige Tools, die in der Analysis hilfreich sind.
+    Hier finden sich einige Tools, die in der Analysis hilfreich sind. Auf der linken Seite findet sich ein Menü mit Links zu verschiedenen Tools.
     """
 )
+
+st.image(image)
