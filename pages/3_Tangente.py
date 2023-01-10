87458878a71db9aaf198b8e0a0a8550962bcc0a5
m-pagel <pagel@max-planck-gymnasium.de>
2023-01-10 20:00:54 +0100

    minus

--- pages/3_Tangente.py
+++ pages/3_Tangente.py
@@ -63,11 +63,11 @@ try:
     st.latex(r'''
         \begin{align*}
             y &= f'(u) \cdot (x - u) +f(u)\\
-            y &= f'(%s) \cdot (x - %s) +f(%s)\\   
-            y &=  %s \cdot (x - %s) + %s\\
+            y &= f'(%s) \cdot (%s) +f(%s)\\   
+            y &=  %s \cdot (%s) + %s\\
             t:y &= %s
         \end{align*}
-    ''' % (sp.latex(u), sp.latex(u), sp.latex(u), sp.latex(f1.subs(x,u)), sp.latex(u), sp.latex(f.subs(x,u)), sp.latex(t))
+    ''' % (sp.latex(u), sp.latex(x-u), sp.latex(x-u), sp.latex(f1.subs(x,u)), sp.latex(u), sp.latex(f.subs(x,u)), sp.latex(t))
     )
     zeichnen = st.checkbox('Zeichnen der Funktionsgraphen?')
     if zeichnen:
